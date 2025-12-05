8d8ec7cebad90bb306fc928f3f45590fd3badcff
iqfar <iqfar.silai@gmail.com>
2025-12-05 18:04:37 +0700

    disable cors

--- src/main/java/com/lastplace/config/SecurityConfig.java
+++ src/main/java/com/lastplace/config/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
